b8ce1f5e71b33b28112cfa8a836f031bb387e414
Smith Ranjan <smith.ranjan07@gmail.com>
2025-08-16 11:32:50 +0530

    Add latest changes

--- backend/index.js
+++ backend/index.js
@@ -18,6 +18,9 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.get("/", (req, res) => {
+  res.send("API is running...");
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/menu", menuRoutes);
 app.use("/api/upload", uploadRoutes); // add upload routes
