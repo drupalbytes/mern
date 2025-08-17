8950fa0117cec6667bebf79d87db6a0e084d3de5
Smith Ranjan <smith.ranjan07@gmail.com>
2025-08-17 10:15:19 +0530

    uploading changes

--- frontend/src/components/AppDownload.jsx
+++ frontend/src/components/AppDownload.jsx
@@ -1,11 +1,50 @@
 
 import React from "react";
+import MobileImg from "../assets/mobile.png";
 
 export default function AppDownload() {
   return (
-    <div className="app-download">
-      <h2>Download Our App</h2>
-      <p>Get our restaurant app for easy ordering and reservations.</p>
-    </div>
+    <section className="py-5 bg-dark text-white">
+      <div className="container">
+        <div className="row align-items-center">
+          
+          {/* Left Side - Text & Buttons */}
+          <div className="col-md-7 text-center text-md-start ps-5">
+            <p className="text-warning fw-semibold mb-2">Download App</p>
+            <h2 className="fw-bold mb-4 text-white">
+              People around you are ordering delicious meals using the &nbsp;
+              <span className="text-white bg-primary px-2 d-inline-block mt-2 skew-bg">
+                 Treact App
+              </span>
+            </h2>
+
+            {/* Buttons */}
+            <div className="d-flex gap-3 justify-content-center justify-content-md-start">
+              <a
+                href="#"
+                className="btn btn-light d-flex align-items-center rounded-pill px-4 py-2"
+              >
+                <i class="fa-brands fa-apple pe-2"></i> APP STORE
+              </a>
+              <a
+                href="#"
+                className="btn btn-light d-flex align-items-center rounded-pill px-4 py-2"
+              >
+                <i class="fa-brands fa-google-play pe-2"></i> GOOGLE PLAY
+              </a>
+            </div>
+          </div>
+
+          {/* Right Side - Mobile Mockup */}
+          <div className="col-md-5 text-center mt-4 mt-md-0">
+            <img
+              src={MobileImg}
+              alt="App Preview"
+              className="img-fluid"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
